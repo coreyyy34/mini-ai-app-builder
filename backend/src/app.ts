69525ad9bfecbd1bef2942b1fa3932271fc3f03c
Corey <cbh344@outlook.com>
2025-09-27 21:09:33 +1200

    setup cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,16 @@
 import express, { Application } from "express";
+import cors from "cors";
 import rootRouter from "./routes";
 
 const app: Application = express();
 
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+	})
+);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
