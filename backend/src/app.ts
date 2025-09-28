e2d3abee8e49f177fe0555aadc538074e5ad0cf5
Corey <cbh344@outlook.com>
2025-09-28 18:20:01 +1300

    configure cors from env variable

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,15 +2,15 @@ import express, { Application } from "express";
 import cors from "cors";
 import rootRouter from "./routes";
 
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
 const app: Application = express();
 
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: CLIENT_URL,
 		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 	})
 );
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
